8706d2dd8cf127aedbfe63b4f8fe3a5d4ac9890f
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2026-01-15 16:50:12 +0200

    remove AuthProvider

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -4,31 +4,19 @@ import { ReactNode } from "react";
 import Providers from "@/lib/providers";
 import Header from "@/components/Header/Header";
 import Sidebar from "@/components/Sidebar/Sidebar";
-import AuthProvider from "@/components/AuthProvider/AuthProvider";
-import { usePathname } from "next/navigation";
 import css from "../layout.module.css";
 
 type Props = {
   children: ReactNode;
 };
 
-const guestRoutes = ["/login", "/register"];
-
 export default function Layout({ children }: Props) {
-  const pathname = usePathname();
-
-  const content = guestRoutes.includes(pathname) ? (
-    children
-  ) : (
-    <AuthProvider>{children}</AuthProvider>
-  );
-
   return (
     <Providers>
       <Sidebar />
       <div className={css.main_container}>
         <Header />
-        <main className="container">{content}</main>
+        <main className="container">{children}</main>
       </div>
     </Providers>
   );
