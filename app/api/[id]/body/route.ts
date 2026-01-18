cc8c013813055fd9cc0ae5ec4fc866d2fba597fa
Stanislav Hanchev <stanislav@smoothr.de>
2026-01-19 00:44:19 +0200

    fix baby and mom body page journey

---
+++ app/api/[id]/body/route.ts
@@ -0,0 +1,32 @@
+import { cookies } from "next/headers";
+import { lehlekaApi } from "../../api";
+import { NextResponse } from "next/server";
+import { isAxiosError } from "axios";
+import { logErrorResponse } from "../../_utils/utils";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function GET(request: Request, { params }: Props) {
+    try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+    const res = await lehlekaApi.get(`/weeks/${id}/body`, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
