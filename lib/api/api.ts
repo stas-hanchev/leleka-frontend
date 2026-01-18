7ebedac2d6947482275fc50d39a311391d49c425
Stanislav Hanchev <stanislav@smoothr.de>
2026-01-19 01:17:58 +0200

    fix env naming

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const baseURL = `${process.env.NEXT_SITE_API_URL}/api` || 'http://localhost:3000/api';
+const baseURL = `${process.env.NEXT_PUBLIC_SITE_API_URL}/api` || 'http://localhost:3000/api';
 
-const baseURL = 'http://localhost:3000/api';
+// const baseURL = 'http://localhost:3000/api';
 
 // axios.defaults.withCredentials = true;
 export const NextServer = axios.create({
