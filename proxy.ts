9598d0846fa282198eb0184b27cb7230cf664cdc
Renat Moroz <bs4123555@gmail.com>
2026-02-23 16:17:33 +0100

    fix-cookie-and-logout-renat

--- proxy.ts
+++ proxy.ts
@@ -9,6 +9,9 @@ const privateRoutes = [
   '/users/avatar',
   '/weeks/dashboard',
   '/weeks/',
+  '/diary',
+  '/journey/:path*',
+  '/profile',
 ];
 
 const publicRoutes = [
@@ -21,69 +24,71 @@ const publicRoutes = [
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  return NextResponse.redirect(new URL('/auth/login', request.url));
+
+  const isPublicRoute = publicRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+  const isPrivateRoute = privateRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  const accessToken = request.cookies.get('accessToken')?.value;
+  const refreshToken = request.cookies.get('refreshToken')?.value;
+
+  // ✅ accessToken є — пускаємо
+  if (accessToken) {
+    if (isPublicRoute) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    return NextResponse.next();
+  }
+
+  // ❌ accessToken нема, але є refreshToken → пробуємо refresh
+  if (!accessToken && refreshToken) {
+    try {
+      const apiRes = await checkServerSession(); // ← бек повертає set-cookie
+
+      const response = isPublicRoute
+        ? NextResponse.redirect(new URL('/', request.url))
+        : NextResponse.next();
+
+      const setCookie = apiRes.headers['set-cookie'];
+      if (setCookie) {
+        const cookiesArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+        cookiesArray.forEach((cookie) => {
+          response.headers.append('set-cookie', cookie);
+        });
+      }
+
+      return response;
+    } catch {
+      // refresh не вдався
+      if (isPrivateRoute) {
+        return NextResponse.redirect(new URL('/auth/login', request.url));
+      }
+      return NextResponse.next();
+    }
+  }
+
+  // ❌ ні access, ні refresh
+  if (isPrivateRoute) {
+    return NextResponse.redirect(new URL('/auth/login', request.url));
+  }
+
+  return NextResponse.next();
 }
 
-//   const isPublicRoute = publicRoutes.some((route) =>
-//     pathname.startsWith(route)
-//   );
-//   const isPrivateRoute = privateRoutes.some((route) =>
-//     pathname.startsWith(route)
-//   );
-
-//   const accessToken = request.cookies.get('accessToken')?.value;
-//   const refreshToken = request.cookies.get('refreshToken')?.value;
-
-//   // ✅ accessToken є — пускаємо
-//   if (accessToken) {
-//     if (isPublicRoute) {
-//       return NextResponse.redirect(new URL('/', request.url));
-//     }
-//     return NextResponse.next();
-//   }
-
-//   // ❌ accessToken нема, але є refreshToken → пробуємо refresh
-//   if (!accessToken && refreshToken) {
-//     try {
-//       const apiRes = await checkServerSession(); // ← бек повертає set-cookie
-
-//       const response = isPublicRoute
-//         ? NextResponse.redirect(new URL('/', request.url))
-//         : NextResponse.next();
-
-//       const setCookie = apiRes.headers['set-cookie'];
-//       if (setCookie) {
-//         const cookiesArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-//         cookiesArray.forEach((cookie) => {
-//           response.headers.append('set-cookie', cookie);
-//         });
-//       }
-
-//       return response;
-//     } catch {
-//       // refresh не вдався
-//       if (isPrivateRoute) {
-//         return NextResponse.redirect(new URL('/auth/login', request.url));
-//       }
-//       return NextResponse.next();
-//     }
-//   }
-
-//   // ❌ ні access, ні refresh
-//   if (isPrivateRoute) {
-//     return NextResponse.redirect(new URL('/auth/login', request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: [
-//     '/auth/:path*',
-//     '/diaries/:path*',
-//     '/tasks/:path*',
-//     '/users/current',
-//     '/users/avatar',
-//     '/weeks/:path*',
-//   ],
-// };
+export const config = {
+  matcher: [
+    '/',
+    '/diary',
+    '/journey/:path*',
+    '/profile',
+    '/auth/:path*',
+    '/diary/:path*',
+    '/tasks/:path*',
+    '/users/current',
+    '/users/avatar',
+    '/weeks/:path*',
+  ],
+};
